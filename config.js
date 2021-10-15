8e580f957578a639fa08f4eaf16c3f5153b107ed
khooii <89780367+khooii@users.noreply.github.com>
2021-10-15 21:21:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,8 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3
+    mess: 'Khooi biết mà 🥰. Athu iu Khooi nhiều nhiều 😘😘',
+    messDesc: 'Tối nay ngủ ngon nka công chúa của mìn :33333.',
+    btnAccept: 'Yeu bb <3
+    https://www.facebook.com/profile.php?id=100073533376934
 }
